ba43e7d6453894e137e7a606a873c86c39b051ec
Thom Leggett <thom@tteggel.org>
2014-04-07 20:41:54 +0000

    Primary look up by project. Project list from OpenStack governance repo.

--- gerrit_monger.py
+++ gerrit_monger.py
@@ -1,9 +1,12 @@
 from random import randint
 from time import sleep
 import argparse
+import pprint
 
 from pymongo import MongoClient
 import gerritlib.gerrit
+import yaml
+import requests
 
 import version
 
@@ -13,7 +16,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-g', '--gerrithost', default='review.openstack.org',
                     help='hostname of the gerrit to crawl.',
                     type=str)
-parser.add_argument('-p', '--gerritport', default=29418,
+parser.add_argument('-i', '--gerritport', default=29418,
                     help='port of the gerrit to crawl.',
                     type=int)
 parser.add_argument('-m', '--mongohost', default='127.0.0.1',
@@ -31,10 +34,8 @@ parser.add_argument('-u', '--gerrituser', default='tteggel',
 parser.add_argument('-r', '--resumesortkey', default='z',
                     help='start at the provided sort key.',
                     type=str)
-parser.add_argument('-s', '--status', 
-                    default=['open', 'reviewed', 'submitted', 'abandoned', 
-                             'merged', 'new'],
-                    help='gerrit statuses to mong.',
+parser.add_argument('-p', '--projects', default=None,
+                    help='projects to mong.',
                     type=str, nargs='*')
 parser.add_argument('-a', '--age', default=None,
                     help=('only return changes that have changed in last '
@@ -50,39 +51,61 @@ gerrit = gerritlib.gerrit.Gerrit(args.gerrithost, args.gerrituser,
 
 ###############################################################################
 
-def get_change_list(status):
+def get_change_list(project, age=None, sort_key=None):
 
-    sortKey = args.resumesortkey
-    last_sortKey = False	
-    while sortKey != last_sortKey:
-        last_sortKey = sortKey
+    last_sort_key = False	
+    while sort_key != last_sort_key:
+        last_sort_key = sort_key
         print(("Getting chages from gerrit - "
-              "status: {0}; sortKey:{1}.").format(status, sortKey))
+              "project: {0}; sortKey: {1}.").format(project, sort_key))
         try:
             changes = gerrit.bulk_query(("--commit-message --comments "
                                      "--current-patch-set --patch-sets "
                                      "--all-approvals --files --dependencies "
                                      "--submit-records limit:100 -- "
-                                     "status:{0} resume_sortkey:{1} "
-                                     "{2}").format(status, sortKey, 
-                                       "-age:{0}".format(args.age) 
-                                         if args.age else ""))
+                                     "project:{0} resume_sortkey:{1} "
+                                     "{2}").format(project, sort_key, 
+                                       "-age:{0}".format(age) 
+                                         if age else ""))
         except Exception, e:
             print(e)
 
         for change in changes:
             if 'id' not in change: continue
-            print('{0}: {1}'.format(
-                change['project'], 
-                change['id']))
+            print('{3} is {2} in {0} at {1}'.format(
+                change['project'], change['sortKey'], change['status'], 
+                change['number']))
             change['_id'] = change['id']
             changedb.save(change)
             if 'sortKey' in change:
-                sortKey = change['sortKey']
+                sort_key = change['sortKey']
         
 	sleep(randint(5, 10))
 
+
+def get_approved_projects():
+    programs_url = 'http://git.openstack.org/cgit/openstack/governance/plain/reference/programs.yaml'
+    y = yaml.load(requests.get(programs_url).text)
+
+    projects = []
+    for _, project in y.items():
+        for _, l in project['projects'].items():
+            for repo in l:
+                projects.append(repo)
+
+    return projects
+    
+
 ###############################################################################
 
-for status in args.status:
-    get_change_list(status)
+projects = []
+if args.projects:
+    projects = args.projects
+else:
+    projects = get_approved_projects() 
+
+pprint.pprint(projects)
+
+for project in projects:
+    get_change_list(project, age=args.age, 
+        sort_key=args.resumesortkey)
